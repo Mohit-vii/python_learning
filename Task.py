706bd7b8996542dea82c6b15d86c1104225c840e
mohit-vii <mohitbharati0711@gmail.com>
2024-04-11 00:43:35 +0530

    Added 2 task

---
+++ Task.py
@@ -0,0 +1,23 @@
+#Kepp entering number until number is between 1 to 10\
+    
+# while(True):   #1
+#     print("Enter number")   
+#     n= int(input())     #43 
+#     if(n>=1 and n<=10): #43>=1 and 43<=10 wrong ,so loop me nahi jaye ga 
+#         print("You entered ",n)
+#         break
+    
+
+# print number in reverse order till 1
+
+print("Enter number")
+number =int(input())
+for i in range(number,0,-1):# hero 0 is not included   , inization, conditon,inc/dec
+    
+    if(i==5):
+        break
+    print(i)
+ 
+
+
+    
